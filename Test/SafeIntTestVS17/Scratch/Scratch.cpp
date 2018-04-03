e131e19673dcd1139cac2d02dce355af15e0947e
David LeBlanc <dleblanc@microsoft.com>
2018-04-03 11:23:17 -0700

    Update scratch project to enable merge

--- Test/SafeIntTestVS17/Scratch/Scratch.cpp
+++ Test/SafeIntTestVS17/Scratch/Scratch.cpp
@@ -1,13 +1,27 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 #include <iostream>
+#define SAFEINT_USE_INTRINSICS 0
+
+#include "../../../SafeInt.hpp"
 
 /*
 Use this to check specific scenarios
 */
 
+unsigned long long Foo(unsigned long long a, unsigned long long b)
+{
+    SafeInt<unsigned long long> x = SafeInt<unsigned long long>(a) * b;
+    return x;
+}
 int main(int argc, char** argv)
 {
-    std::cout << __cpp_constexpr << std::endl;
+    unsigned long long a = argc;
+    unsigned long long b = 23;
+
+    unsigned long long x = Foo(a, b);
+    if (x > 300)
+        printf("Foo\n");
+
     return 0;
 }
