74b078dd790822eb7fb77d1e5a0c11fa0348119b
David LeBlanc <dcl@dleblanc.net>
2022-04-23 23:53:44 -0700

    Switch to CMake, enable build of gcc and clang with one build, set up tests

---
+++ Test/ClangTest/readme.txt
@@ -0,0 +1,2 @@
+The makefile has been phased out, and replaced with CMakeLists.txt.
+It will be left as is for those who prefer make over cmake, but it will no longer be maintained.
--- Test/TestMain.cpp
+++ Test/TestMain.cpp
@@ -9,6 +9,30 @@
 
 int main(int, char**)
 {
+    #if SAFEINT_USE_INTRINSICS == 0
+    std::cout << "Testing without intrinsics" << std::endl;
+    #else
+    std::cout << "Testing with intrinsics" << std::endl;
+    #endif
+
+    #if SAFEINT_HAS_INT128 == 1
+    std::cout << "Testing using int128" << std::endl;
+    #else
+    std::cout << "Testing not using int128" << std::endl;
+    #endif
+
+    #if SAFEINT_COMPILER == GCC_COMPILER 
+    std::cout << "Compiler = g++" << std::endl;
+    #endif
+    
+    #if SAFEINT_COMPILER == CLANG_COMPILER
+    std::cout << "Compiler = clang" << std::endl;
+    #endif
+
+    #if SAFEINT_COMPILER == VISUAL_STUDIO_COMPILER
+    std::cout << "Compiler = Visual Studio" << std::endl;
+    #endif
+
     cast_verify::CastVerify();
     mult_verify::MultVerify();
     div_verify::DivVerify();
