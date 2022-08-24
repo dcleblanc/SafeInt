a1490583d65e29b93a955341f68cac7d8df22232
David LeBlanc <dleblanc@microsoft.com>
2022-08-24 13:29:33 -0700

    Add more test compile cases, div and sub remaiing

--- Test/c_safe_math/compile_test.c
+++ Test/c_safe_math/compile_test.c
@@ -305,3 +305,146 @@ void check_add_compile(void)
 		ret = check_add_ulonglong_longlong(arg_ullong, arg_llong, &arg_ullong);
 		ret = check_add_ulonglong_ulonglong(arg_ullong, arg_ullong, &arg_ullong);
 }
+
+void safe_mul_compile(void)
+{
+	int ret_int;
+	unsigned int ret_uint;
+	long ret_long;
+	unsigned long ret_ulong;
+	long long ret_llong;
+	unsigned long long ret_ullong;
+	int arg_int = 1;
+	unsigned int arg_uint = 1;
+	long arg_long = 1;
+	unsigned long arg_ulong = 1;
+	long long arg_llong = 1;
+	unsigned long long arg_ullong = 1;
+
+	ret_int = safe_mul_int_int(arg_int, arg_int);
+	ret_int = safe_mul_int_uint(arg_int, arg_uint);
+	ret_int = safe_mul_int_longlong(arg_int, arg_llong);
+	ret_int = safe_mul_int_ulonglong(arg_int, arg_ullong);
+	ret_uint = safe_mul_uint_int(arg_uint, arg_int);
+	ret_uint = safe_mul_uint_uint(arg_uint, arg_uint);
+	ret_uint = safe_mul_uint_longlong(arg_uint, arg_llong);
+	ret_uint = safe_mul_uint_ulonglong(arg_uint, arg_ullong);
+	ret_int = safe_mul_int_long(arg_int, arg_long);
+	ret_int = safe_mul_int_ulong(arg_int, arg_ulong);
+	ret_uint = safe_mul_uint_long(arg_uint, arg_long);
+	ret_uint = safe_mul_uint_ulong(arg_uint, arg_ulong);
+	ret_long = safe_mul_long_int(arg_long, arg_int);
+	ret_long = safe_mul_long_uint(arg_long, arg_uint);
+	ret_long = safe_mul_long_long(arg_long, arg_long);
+	ret_long = safe_mul_long_ulong(arg_long, arg_ulong);
+	ret_long = safe_mul_long_longlong(arg_long, arg_llong);
+	ret_long = safe_mul_long_ulonglong(arg_long, arg_ullong);
+	ret_ulong = safe_mul_ulong_int(arg_ulong, arg_int);
+	ret_ulong = safe_mul_ulong_uint(arg_ulong, arg_uint);
+	ret_ulong = safe_mul_ulong_long(arg_ulong, arg_long);
+	ret_ulong = safe_mul_ulong_ulong(arg_ulong, arg_ulong);
+	ret_ulong = safe_mul_ulong_longlong(arg_ulong, arg_llong);
+	ret_ulong = safe_mul_ulong_ulonglong(arg_ulong, arg_ullong);
+	ret_llong = safe_mul_longlong_long(arg_llong, arg_long);
+	ret_llong = safe_mul_longlong_ulong(arg_llong, arg_ulong);
+	ret_ullong = safe_mul_ulonglong_long(arg_ullong, arg_long);
+	ret_ullong = safe_mul_ulonglong_ulong(arg_ullong, arg_ulong);
+	ret_int = safe_mul_int_long(arg_int, arg_long);
+	ret_int = safe_mul_int_ulong(arg_int, arg_ulong);
+	ret_uint = safe_mul_uint_long(arg_uint, arg_long);
+	ret_uint = safe_mul_uint_ulong(arg_uint, arg_ulong);
+	ret_long = safe_mul_long_int(arg_long, arg_int);
+	ret_long = safe_mul_long_uint(arg_long, arg_uint);
+	ret_long = safe_mul_long_long(arg_long, arg_long);
+	ret_long = safe_mul_long_ulong(arg_long, arg_ulong);
+	ret_long = safe_mul_long_longlong(arg_long, arg_llong);
+	ret_long = safe_mul_long_ulonglong(arg_long, arg_ullong);
+	ret_ulong = safe_mul_ulong_int(arg_ulong, arg_int);
+	ret_ulong = safe_mul_ulong_uint(arg_ulong, arg_uint);
+	ret_ulong = safe_mul_ulong_long(arg_ulong, arg_long);
+	ret_ulong = safe_mul_ulong_ulong(arg_ulong, arg_ulong);
+	ret_ulong = safe_mul_ulong_longlong(arg_ulong, arg_llong);
+	ret_ulong = safe_mul_ulong_ulonglong(arg_ulong, arg_ullong);
+	ret_llong = safe_mul_longlong_long(arg_llong, arg_long);
+	ret_llong = safe_mul_longlong_ulong(arg_llong, arg_ulong);
+	ret_ullong = safe_mul_ulonglong_long(arg_ullong, arg_long);
+	ret_ullong = safe_mul_ulonglong_ulong(arg_ullong, arg_ulong);
+	ret_llong = safe_mul_longlong_int(arg_llong, arg_int);
+	ret_llong = safe_mul_longlong_uint(arg_llong, arg_uint);
+	ret_llong = safe_mul_longlong_longlong(arg_llong, arg_llong);
+	ret_llong = safe_mul_longlong_ulonglong(arg_llong, arg_ullong);
+	ret_ullong = safe_mul_ulonglong_int(arg_ullong, arg_int);
+	ret_ullong = safe_mul_ulonglong_uint(arg_ullong, arg_uint);
+	ret_ullong = safe_mul_ulonglong_longlong(arg_ullong, arg_llong);
+	ret_ullong = safe_mul_ulonglong_ulonglong(arg_ullong, arg_ullong);
+
+}
+
+void check_mul_compile(void)
+{
+	bool ret;
+	int arg_int = 1;
+	unsigned int arg_uint = 1;
+	long arg_long = 1;
+	unsigned long arg_ulong = 1;
+	long long arg_llong = 1;
+	unsigned long long arg_ullong = 1;
+
+	ret = check_mul_int_int(arg_int, arg_int, &arg_int);
+	ret = check_mul_int_uint(arg_int, arg_uint, &arg_int);
+	ret = check_mul_int_longlong(arg_int, arg_llong, &arg_int);
+	ret = check_mul_int_ulonglong(arg_int, arg_ullong, &arg_int);
+	ret = check_mul_uint_int(arg_uint, arg_int, &arg_uint);
+	ret = check_mul_uint_uint(arg_uint, arg_uint, &arg_uint);
+	ret = check_mul_uint_longlong(arg_uint, arg_llong, &arg_uint);
+	ret = check_mul_uint_ulonglong(arg_uint, arg_ullong, &arg_uint);
+	ret = check_mul_int_long(arg_int, arg_long, &arg_int);
+	ret = check_mul_int_ulong(arg_int, arg_ulong, &arg_int);
+	ret = check_mul_uint_long(arg_uint, arg_long, &arg_uint);
+	ret = check_mul_uint_ulong(arg_uint, arg_ulong, &arg_uint);
+	ret = check_mul_long_int(arg_long, arg_int, &arg_long);
+	ret = check_mul_long_uint(arg_long, arg_uint, &arg_long);
+	ret = check_mul_long_long(arg_long, arg_long, &arg_long);
+	ret = check_mul_long_ulong(arg_long, arg_ulong, &arg_long);
+	ret = check_mul_long_longlong(arg_long, arg_llong, &arg_long);
+	ret = check_mul_long_ulonglong(arg_long, arg_ullong, &arg_long);
+	ret = check_mul_ulong_int(arg_ulong, arg_int, &arg_ulong);
+	ret = check_mul_ulong_uint(arg_ulong, arg_uint, &arg_ulong);
+	ret = check_mul_ulong_long(arg_ulong, arg_long, &arg_ulong);
+	ret = check_mul_ulong_ulong(arg_ulong, arg_ulong, &arg_ulong);
+	ret = check_mul_ulong_longlong(arg_ulong, arg_llong, &arg_ulong);
+	ret = check_mul_ulong_ulonglong(arg_ulong, arg_ullong, &arg_ulong);
+	ret = check_mul_longlong_long(arg_llong, arg_long, &arg_llong);
+	ret = check_mul_longlong_ulong(arg_llong, arg_ulong, &arg_llong);
+	ret = check_mul_ulonglong_long(arg_ullong, arg_long, &arg_ullong);
+	ret = check_mul_ulonglong_ulong(arg_ullong, arg_ulong, &arg_ullong);
+	ret = check_mul_int_long(arg_int, arg_long, &arg_int);
+	ret = check_mul_int_ulong(arg_int, arg_ulong, &arg_int);
+	ret = check_mul_uint_long(arg_uint, arg_long, &arg_uint);
+	ret = check_mul_uint_ulong(arg_uint, arg_ulong, &arg_uint);
+	ret = check_mul_long_int(arg_long, arg_int, &arg_long);
+	ret = check_mul_long_uint(arg_long, arg_uint, &arg_long);
+	ret = check_mul_long_long(arg_long, arg_long, &arg_long);
+	ret = check_mul_long_ulong(arg_long, arg_ulong, &arg_long);
+	ret = check_mul_long_longlong(arg_long, arg_llong, &arg_long);
+	ret = check_mul_long_ulonglong(arg_long, arg_ullong, &arg_long);
+	ret = check_mul_ulong_int(arg_ulong, arg_int, &arg_ulong);
+	ret = check_mul_ulong_uint(arg_ulong, arg_uint, &arg_ulong);
+	ret = check_mul_ulong_long(arg_ulong, arg_long, &arg_ulong);
+	ret = check_mul_ulong_ulong(arg_ulong, arg_ulong, &arg_ulong);
+	ret = check_mul_ulong_longlong(arg_ulong, arg_llong, &arg_ulong);
+	ret = check_mul_ulong_ulonglong(arg_ulong, arg_ullong, &arg_ulong);
+	ret = check_mul_longlong_long(arg_llong, arg_long, &arg_llong);
+	ret = check_mul_longlong_ulong(arg_llong, arg_ulong, &arg_llong);
+	ret = check_mul_ulonglong_long(arg_ullong, arg_long, &arg_ullong);
+	ret = check_mul_ulonglong_ulong(arg_ullong, arg_ulong, &arg_ullong);
+	ret = check_mul_longlong_int(arg_llong, arg_int, &arg_llong);
+	ret = check_mul_longlong_uint(arg_llong, arg_uint, &arg_llong);
+	ret = check_mul_longlong_longlong(arg_llong, arg_llong, &arg_llong);
+	ret = check_mul_longlong_ulonglong(arg_llong, arg_ullong, &arg_llong);
+	ret = check_mul_ulonglong_int(arg_ullong, arg_int, &arg_ullong);
+	ret = check_mul_ulonglong_uint(arg_ullong, arg_uint, &arg_ullong);
+	ret = check_mul_ulonglong_longlong(arg_ullong, arg_llong, &arg_ullong);
+	ret = check_mul_ulonglong_ulonglong(arg_ullong, arg_ullong, &arg_ullong);
+
+}
